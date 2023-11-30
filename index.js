02224db71a3338e8d1687100c5f1495ba5a5fb52
luddilo <ludvig.linse@gmail.com>
2023-11-30 15:30:57 +0200

    TS attempt

--- index.js
+++ index.js
@@ -9,25 +9,25 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
 // `data` is the data you encrypted and passed into `evervault.run` from your server. The Function
 // automatically decrypts the data and maintains its structure so you can treat event exactly as
 // you did when you passed it into `evervault.run`.
-function handler(data, context) {
-    return __awaiter(this, void 0, void 0, function* () {
-        // Check if the data sent into the Function included the `name` key
-        if (data.name && typeof data.name === "string") {
-            console.debug(`A name of length ${data.name.length} has arrived into the Function.`);
-            // Process the decrypted name value, and re-encrypt the original name using the encrypt function available in the context parameter.
-            return {
-                message: `Hello from a Typescript Function! It seems you have ${data.name.length} letters in your name`,
-            };
-        }
-        else {
-            console.debug("An empty name has arrived into the Function.");
-            return {
-                message: "Hello from a Function! Send an encrypted `name` parameter to show Function decryption in action",
-            };
-        }
-    });
-}
-exports.default = handler;
+const handler = (data, context) => __awaiter(void 0, void 0, void 0, function* () {
+    // Check if the data sent into the Function included the `name` key
+    if (data.name && typeof data.name === "string") {
+        console.debug(`A name of length ${data.name.length} has arrived into the Function.`);
+        // Process the decrypted name value, and re-encrypt the original name using the encrypt function available in the context parameter.
+        return {
+            message: `Hello from a Function! It seems you have ${data.name.length} letters in your name`,
+            name: context.encrypt(data.name),
+        };
+    }
+    else {
+        console.debug("An empty name has arrived into the Function.");
+        return {
+            message: "Hello from a Function! Send an encrypted `name` parameter to show Function decryption in action",
+        };
+    }
+});
+exports.handler = handler;
--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 // `data` is the data you encrypted and passed into `evervault.run` from your server. The Function
 // automatically decrypts the data and maintains its structure so you can treat event exactly as
 // you did when you passed it into `evervault.run`.
-export async function handler(data: any, context: any) {
+export const handler = async (data: any, context: any) => {
   // Check if the data sent into the Function included the `name` key
   if (data.name && typeof data.name === "string") {
     console.debug(
@@ -10,7 +10,8 @@ export async function handler(data: any, context: any) {
 
     // Process the decrypted name value, and re-encrypt the original name using the encrypt function available in the context parameter.
     return {
-      message: `Hello from a Typescript Function! It seems you have ${data.name.length} letters in your name`,
+      message: `Hello from a Function! It seems you have ${data.name.length} letters in your name`,
+      name: context.encrypt(data.name),
     };
   } else {
     console.debug("An empty name has arrived into the Function.");
@@ -20,4 +21,4 @@ export async function handler(data: any, context: any) {
         "Hello from a Function! Send an encrypted `name` parameter to show Function decryption in action",
     };
   }
-}
+};
