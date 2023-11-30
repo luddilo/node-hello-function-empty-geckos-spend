49c951d9369a413e0497fff01526d7df8b5157e1
luddilo <ludvig.linse@gmail.com>
2023-11-30 16:03:20 +0200

    added lib properly

--- dist/other.js
+++ dist/other.js
@@ -1,4 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.otherStr = void 0;
-exports.otherStr = "LALALA";
+const narratory_1 = require("narratory");
+exports.otherStr = narratory_1.DELAY_AFTER_TRAINING;
